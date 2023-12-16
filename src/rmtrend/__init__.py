1b3bffd8fa6055c1fca0da2e72a8e09319076275
AZLoFlight <144965782+AZLoFlight@users.noreply.github.com>
2023-12-16 10:25:59 +0800

    Update __init__.py

--- src/rmtrend/__init__.py
+++ src/rmtrend/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("rmtrend")
+
+from rmtrend import fetch_character_info
+from rmtrend import fetch_location_info
+from rmtrend import recent_reddit
